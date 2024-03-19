2477ef56a8b237c6d574f1829f3b163108d3f6a0
gideon <gideonmunene0@gmail.com>
2024-03-19 13:01:17 -0400

    flow

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
+  let result
+  if (someValue=199){
+    return("This one is on me!")
+  }
+  else if (someValue =400<=2000){
+  return("That will be twenty bucks")
+  }
+  else if
+     (someValue>2000){
+     return("I will gladly take your thirty bucks.")
+  }
+  else
+  (someValue>2500)
+return("No can do.")
   // Write your code here!
 }
 
 function ternaryCheckCity(){
+  if ('when the city is NYC'){
+    return("Ok, sounds good.")
+  }else if('if the destination city is not NYC'){
+    return("No go.")
+  }
   // Write your code here!
 }
 
 function switchOnCharmFromTip(){
+  if('generous'){
+  return("Thank you so much.")}
+else if('not as generous'){
+return("Thank you.")}
+
+else('if anything else')
+{return("Bye.")}
   // Write your code here!
 }
